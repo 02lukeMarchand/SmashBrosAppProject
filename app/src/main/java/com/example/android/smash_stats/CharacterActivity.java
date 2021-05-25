97334b28405dd9282a5cef545f0348616629e785
Luke <02luke.marchand@gulllakecs.org>
2021-05-25 12:29:58 -0400

    Character weight appears on character screen
    @+id/character_weight

--- app/src/main/java/com/example/android/smash_stats/CharacterActivity.java
+++ app/src/main/java/com/example/android/smash_stats/CharacterActivity.java
@@ -3,7 +3,10 @@ package com.example.android.smash_stats;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class CharacterActivity extends AppCompatActivity {
 
@@ -16,13 +19,16 @@ public class CharacterActivity extends AppCompatActivity {
         String[] names = getResources().getStringArray(R.array.characters);
         String[] numbers = getResources().getStringArray(R.array.numbers);
         String[] sizes = getResources().getStringArray(R.array.size_stats);
+        String[] weights = getResources().getStringArray(R.array.weight_stats);
 
         TextView characterNameView = (TextView) findViewById(R.id.character_name);
         TextView numberView = (TextView) findViewById(R.id.character_number);
         TextView sizeView = (TextView) findViewById(R.id.character_size);
+        TextView weightView = (TextView) findViewById(R.id.character_weight);
 
         characterNameView.setText(names[MainActivity.charSelected]);
         numberView.setText(numbers[MainActivity.charSelected]);
         sizeView.setText(sizes[MainActivity.charSelected]);
+        weightView.setText(weights[MainActivity.charSelected]);
     }
 }
